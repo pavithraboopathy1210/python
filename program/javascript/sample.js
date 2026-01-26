c03a5a33abf1b864d891cd4bdeb59bcece45dd8b
pavithraboopathy1210 <pavithraboopathi60@gmail.com>
2026-01-26 17:45:09 +0530

    Delete program/javascript directory

--- program/javascript/sample.js
+++
@@ -1,9 +0,0 @@
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-readline.question("Enter a number: ", x => {
-  console.log("Result:", parseInt(x) * 10);
-  readline.close();
-});
