7832396bcf14a19509e97bf60f4d81584a11d26f
pavithraboopathy1210 <pavithraboopathi60@gmail.com>
2026-01-26 17:44:00 +0530

    Delete program/JAVA directory

--- program/JAVA/Helo.java
+++
@@ -1,7 +0,0 @@
-public class Helo{
-
-    public static void main(String[] args) {
-    System.out.println("hasik");
-
-    }
-}
--- program/JAVA/Summa.java
+++
@@ -1,7 +0,0 @@
-
-public class Summa{
-    public static void main(String[] args) {
-        System.out.println("this is pavithra");
-        Sample obj=new Sample();
-    }
-}
